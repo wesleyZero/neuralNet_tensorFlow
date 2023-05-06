951cce43b988f11097a11d8d48617b09de9b03ef
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-05-06 11:16:03 -0700

    downloaded the MNIST dataset using keras datasets and noramlized the data

--- src/tf_nn.py
+++ src/tf_nn.py
@@ -1,6 +1,14 @@
 import tensorflow as tf 
 import matplotlib.pyplot as plt 
 from tensorflow.keras import layers 
+import numpy as np
 
+(x_train, y_train), (x_val, y_val) = tf.keras.datasets.mnist.load_data()
+
+# Normalization of the dataset. 
+# The data is 8-bit, normalized to a floating point value for the NN
+
+x_train, y_train, x_val, y_val = x_train / 255, y_train / 255, \
+                                    x_val / 255, y_val / 255
 
 print("The program ran")
