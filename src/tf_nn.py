fdf68ae1aabd4a4e41dbb7aadecc05e635a4e530
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-05-06 12:58:43 -0700

    printed out a test image, saw what the data looked like. I'm going to work on the model now

--- src/tf_nn.py
+++ src/tf_nn.py
@@ -6,9 +6,19 @@ import numpy as np
 (x_train, y_train), (x_val, y_val) = tf.keras.datasets.mnist.load_data()
 
 # Normalization of the dataset. 
-# The data is 8-bit, normalized to a floating point value for the NN
+# The data is 8-bit, normalized to a floating point value for the NN 
+# The y-data is not normalized because it's just the integer labels
+x_train = x_train / 255
+x_val = x_val / 255
 
-x_train, y_train, x_val, y_val = x_train / 255, y_train / 255, \
-                                    x_val / 255, y_val / 255
+# Let's look at an image from the dataset 
+print(type(x_train))
+print(x_train.shape)
+print(y_train.shape)
+print(y_train[0])
+plt.imshow(x_train[0], cmap='hot')
+plt.show()
+
+
 
 print("The program ran")
