bfc7f73a2d5487addcf46842506b9fad232037d8
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-05-06 13:35:49 -0700

    I got the model to start training from the dataset. Will calculate accuracy now

--- src/tf_nn.py
+++ src/tf_nn.py
@@ -16,9 +16,21 @@ print(type(x_train))
 print(x_train.shape)
 print(y_train.shape)
 print(y_train[0])
-plt.imshow(x_train[0], cmap='hot')
-plt.show()
+# plt.imshow(x_train[0], cmap='hot')
+# plt.show()
 
+layer_input = layers.Input(x_train.shape[1:])
+layer_hidden_0 = layers.Flatten()
+layer_output = layers.Dense(10, activation='softmax')
+
+nn_layers = [layer_input, layer_hidden_0, layer_output]
+nn_model = tf.keras.models.Sequential(nn_layers)
+
+nn_model.compile(optimizer='adam', loss='categorical_crossentropy')
+print(nn_model.summary())
+
+one_hot_y_train = tf.one_hot(y_train, 10)
+nn_model.fit(x_train, one_hot_y_train)
 
 
 print("The program ran")
